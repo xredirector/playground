2d8face45565ccad7cc7d39ce169d384ae839ab7
XR <6436450+xredirector@users.noreply.github.com>
2018-04-13 08:41:15 -0400

    First commit

---
+++ program.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# make print compatible with python 2.7
+from __future__ import print_function
+
+"""
+Created on Mon Mar 12 21:11:54 2018
+"""
+
+import argparse
+import fileinput
+import re
+import sys
+
+
+def is_greater (var1, var2):
+    return var1 > var2
+
+if __name__ == "__main__":
+    print(is_greater(3,1))
+
